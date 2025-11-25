21a53df8e6eef59b429f410bd8eca548bcc22d49
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2025-11-24 22:52:21 -0800

    updated dm profile to work with example data, will need to fix example data to be JSON instead of direct js array of objects

--- src/DMProfile.jsx
+++ src/DMProfile.jsx
@@ -1,10 +1,10 @@
-function DMProfile(){
+function DMProfile(dm){
     return (
         <div>
             <h1>Dungeon Master</h1>
             <ul>
-                <li><section className="field">Name: </section> name goes here</li>
-                <li><section className="field">Location: </section>location goes here</li>
+                <li><section className="field">Name: </section>{dm.name}</li>
+                <li><section className="field">Location: </section>{dm.location}</li>
                 <li><section className="field">Pronouns:</section>pronouns go here</li>
                 <li><section className="field">Scheduling Availability:</section>schedule goes here</li>
             </ul>
