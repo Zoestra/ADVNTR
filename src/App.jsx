087d3be3863c67a7e39e20857d31f2de709a4d13
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2025-11-24 22:49:09 -0800

    Filter DMs by location

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import DMProfile from "./DMProfile";
 import PlayerProfile from "./PlayerProfile";
 import CampaignPost from "./campaignPost";
+import DM_data from "./DM_data.js";
 
 function App(){
+    const [locationFilter, setLocationFilter] = useState("");
+    
+    const dm_filter = DM_data.filter(
+        dm => dm.location.toLowerCase().includes(locationFilter.toLowerCase()));
 
+    {/* Assuming campaigns, DMs, and players will be stored as an array.
+        We would filter on the back-end if this system were handling large data-sets.
+        Maybe back-end filtering as a stretch-goal? */}
     return(
         <div>
             <h1>ADVNTR</h1>
@@ -12,9 +21,11 @@ function App(){
                 <li><CampaignPost /></li>
             </ul>
             <h2>Dungeon Masters</h2>
-            <ul>
-                <li><DMProfile /></li>
-            </ul>
+            <input type="text" placeholder="search by location" value={locationFilter} 
+                onChange={(event) => setLocationFilter(event.target.value)}></input>
+                { dm_filter.map( dm => (
+                    <DMProfile key={dm.id} dm={dm} />
+                )) }
             <h2>Players</h2>
             <ul>
                 <li>< PlayerProfile /></li>
---
+++ src/DM_data.js
@@ -0,0 +1,18 @@
+/* An array of DM objects as temporary example data until backend is done */
+export const DM_data = [
+    {
+        id: 1,
+        Name:"Alice",
+        Location: "Seattle",
+    },
+    {
+        id: 2,
+        Name: "Bob",
+        Location: "Tacoma",
+    },
+    {
+        id: 3,
+        Name: "Carol",
+        Location: "Portland",
+    }
+];
