db73711df9beca9db9a2aedbb87ef3f0bddffb64
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2025-11-25 21:38:16 -0800

    Only need one location filter, this simplifies code to facilitate adding other filters

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,32 @@ import {Player_data} from "./Player_data.js";
 import {Campaign_data} from "./Campaign_data.js";
 
 function App(){
-    const [locationFilterDM, setLocationFilterDM] = useState("");
-    const [locationFilterPlayer, setLocationFilterPlayer] = useState("");
-    const [locationFilterCampaign, setLocationFilterCampaign] = useState("");
+    const [locationFilter, setLocationFilter] = useState("");
+    const [schedulingFilter, setSchedulingFilter] = useState(null);
     
     const dm_filter = DM_data.filter(
-        dm => dm.location.toLowerCase().includes(locationFilterDM.toLowerCase()));
+        dm => dm.location.toLowerCase().includes(locationFilter.toLowerCase()));
 
     const player_filter = Player_data.filter(
-        player => player.location.toLowerCase().includes(locationFilterPlayer.toLowerCase()));
+        player => player.location.toLowerCase().includes(locationFilter.toLowerCase()));
 
     const campaign_filter= Campaign_data.filter(
-        campaign => campaign.location.toLowerCase().includes(locationFilterCampaign.toLowerCase()));
+        campaign => campaign.location.toLowerCase().includes(locationFilter.toLowerCase()));
 
     return(
         <div>
             <h1>ADVNTR</h1>
+            <input type="text" placeholder="search by location" value={locationFilter}
+                onChange={(event) => setLocationFilter(event.target.value)}></input>
             <h2>Campaigns</h2>
-            <input type="text" placeholder="search by location" value={locationFilterCampaign}
-                onChange={(event) => setLocationFilterCampaign(event.target.value)}></input>
                 {campaign_filter.map(campaign => (
                     <CampaignPost key={campaign.id} campaign={campaign} />
                 ))}
             <h2>Dungeon Masters</h2>
-            <input type="text" placeholder="search by location" value={locationFilterDM} 
-                onChange={(event) => setLocationFilterDM(event.target.value)}></input>
                 { dm_filter.map( dm => (
                     <DMProfile key={dm.id} dm={dm} />
                 )) }
             <h2>Players</h2>
-            <input type="text" placeholder="search by location" value={locationFilterPlayer}
-                onChange={(event) => setLocationFilterPlayer(event.target.value)}></input>
                 { player_filter.map( player => (
                     <PlayerProfile key={player.id} player={player} />
                 ))}
