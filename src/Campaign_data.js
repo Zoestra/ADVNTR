4fae67a35c6748e50e6e2acdfb581bffcf3343fe
mbokros1 <155133868+mbokros1@users.noreply.github.com>
2025-11-25 23:11:24 -0800

    DM's name is now visible on campaign search results

--- src/Campaign_data.js
+++ src/Campaign_data.js
@@ -4,17 +4,20 @@ export const Campaign_data = [
         name:"Drums from the deep",
         location:"Seattle, WA",
         schedule: "Saturday evening",
+        dm: 1
     },
     {
         id:1002,
         name:"Verdant labyrinth",
         location:"Tacoma, WA",
         schedule: "Friday nights",
+        dm: 2
     },
     {
         id:1003,
         name:"The shattered throne",
         location:"Portland, OR",
         schedule: "Saturday evening",
+        dm: 3
     },
 ];
--- src/campaignPost.jsx
+++ src/campaignPost.jsx
@@ -1,10 +1,11 @@
+import { DM_data } from "./DM_data";
 function CampaignPost({campaign}){
     return(
          <div>
             <ul>
                 <li><section className="field">Name: </section> {campaign.name}</li>
                 <li><section className="field">Location: </section>{campaign.location}</li>
-                <li><section className="field">DM:</section>will link DM profile</li>
+                <li><section className="field">DM:</section>{ DM_data.find(dm => dm.id === campaign.dm).name }</li>
                 <li><section className="field">Theme/Setting:</section>setting or theme goes here</li>
             </ul>
         </div>
